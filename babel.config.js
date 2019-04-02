506541093f0afc229d1450e7d0a318c7ae5744cb
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-04-02 12:17:42 +0500

    fix(babel): Removed @babel/env

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,6 @@ module.exports = function() {
 			'@babel/proposal-object-rest-spread',
     ],
 		presets: [
-			'@babel/env',
 			'@babel/react',
 			'@babel/typescript',
 			'module:metro-react-native-babel-preset'
