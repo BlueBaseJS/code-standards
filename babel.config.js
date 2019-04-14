ea4488800acce2b494a981b8c782e0ad6c103672
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-04-14 17:01:46 +0500

    fix(babel): fixed duplicate plugins

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,7 @@ module.exports = function() {
     plugins: [
       '@babel/proposal-class-properties',
 			'@babel/proposal-object-rest-spread',
-			'@babel/plugin-transform-flow-strip-types',
-			'@babel/plugin-proposal-class-properties',
+			'@babel/transform-flow-strip-types',
     ],
 		presets: [
 			'@babel/react',
