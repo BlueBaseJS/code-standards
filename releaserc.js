50d3a9445c05c88664a652a5492c32a0c19fa46c
Abubakar Saddique <abubakar.mani1@gmail.com>
2019-09-06 15:26:43 +0500

    feat(releaserc): semantic-release-git plugin fork to *-git-branches

--- releaserc.js
+++ releaserc.js
@@ -1,22 +1,36 @@
 module.exports = {
-	"branch": "master",
-	"plugins": [
-		"@semantic-release/commit-analyzer",
-		"@semantic-release/release-notes-generator",
-		"@semantic-release/changelog",
-    ["@semantic-release/exec", {
-      "prepareCmd": "yarn fix:md"
-    }],
-		"@semantic-release/npm",
-		"./scripts/version.js",
-		[
-			"@semantic-release/git",
-			{
-				"assets": ["src/version.ts", "CHANGELOG.md", "package.json"],
-				"message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-			}
-		],
-		"@semantic-release/github",
-		"@qiwi/semantic-release-gh-pages-plugin"
-	]
-}
+  branch: "master",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd: "yarn fix:md"
+      }
+    ]
+  ],
+  verifyConditions: [
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    "semantic-release-git-branches"
+  ],
+  prepare: [
+    "@semantic-release/npm",
+    "./scripts/version.js",
+    {
+      path: "semantic-release-git-branches",
+      assets: ["src/version.ts", "CHANGELOG.md", "package.json"],
+      message:
+        "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }
+  ],
+
+  publish: [
+    "@semantic-release/github",
+    "@qiwi/semantic-release-gh-pages-plugin"
+  ],
+  success: "@semantic-release/github",
+  fail: "@semantic-release/github"
+};
