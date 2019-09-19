5b2b31480f18559cfeddd0d455c6453cb9df93be
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-09-19 23:01:49 +0500

    feat(releaserc): Removed GitHub Page deployment in Semantic Release
    BREAKING CHANGE: Removed GitHub Page deployment in Semantic Release

--- releaserc.js
+++ releaserc.js
@@ -1,31 +1,32 @@
 module.exports = {
-  branches: [
-    "master",
-    "next",
-    { branch: "beta", prerelease: true },
-    { branch: "alpha", prerelease: true }
-  ],
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    [
-      "@semantic-release/exec",
-      {
-        prepareCmd: "yarn fix:md"
-      }
-    ],
-    "@semantic-release/npm",
-    "./scripts/version.js",
-    [
-      "@semantic-release/git",
-      {
-        assets: ["src/version.ts", "CHANGELOG.md", "package.json"],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }
-    ],
-    "@semantic-release/github",
-    "@qiwi/semantic-release-gh-pages-plugin"
-  ]
+	// https://github.com/semantic-release/evolution/blob/release-workflows/_drafts/release-workflows.md#pre-releases
+	branches: [
+		'master',
+		'next',
+		{ branch: 'beta', prerelease: true },
+		{ branch: 'alpha', prerelease: true },
+	],
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/changelog',
+		[
+			'@semantic-release/exec',
+			{
+				prepareCmd: 'yarn fix:md',
+			},
+		],
+		'@semantic-release/npm',
+		'./scripts/version.js',
+		[
+			'@semantic-release/git',
+			{
+				assets: ['src/version.ts', 'CHANGELOG.md', 'package.json'],
+				message:
+					'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+			},
+		],
+		'@semantic-release/github',
+		// "@qiwi/semantic-release-gh-pages-plugin"
+	],
 };
