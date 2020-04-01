81d8299c503c6f34cd7d3c4f2a33807552c74756
Hashim Sohail <hashimsohail21@gmail.com>
2020-04-01 15:22:13 +0500

    feat(bot): Rocket Chat Release Message Channel  (#140)

---
+++ backend/releaserc.js
@@ -0,0 +1,24 @@
+module.exports = {
+	branches: [
+		'master',
+		'next',
+		{ name: 'beta', prerelease: true },
+		{ name: 'alpha', prerelease: true },
+	],
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/changelog',
+		'@semantic-release/npm',
+		'../scripts/postMessage.js',
+		[
+			'@semantic-release/git',
+			{
+				assets: ['package.json', 'CHANGELOG.md',],
+				message:
+					'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+			},
+		],
+		'@semantic-release/github',
+	],
+}
