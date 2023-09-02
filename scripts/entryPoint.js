9e7ad401ffcb9b98a5e13e91868c54804c66a13c
Abdul Rehman Talat <rehman.talat@gmail.com>
2023-09-02 23:51:38 +0500

    fix: Fixed entryPoint

--- scripts/entryPoint.js
+++ scripts/entryPoint.js
@@ -1,11 +1,24 @@
-const fs = require('fs');
+// const fs = require('fs');
+const replace = require('replace-in-file');
 
 async function prepare() {
-	console.log('Updating main property in package.json');
-	const packageJson = require('../package.json');
-	packageJson.main = 'dist/index.js';
-	console.log('Writing package.json');
-	fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
+	// console.log('Updating main property in package.json');
+	// const packageJson = require('../package.json');
+	// packageJson.main = 'dist/index.js';
+	// console.log('Writing package.json');
+	// fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
+
+	updateFiles();
 }
 
+async function updateFiles() {
+	// Update main property in package.json
+	replace.sync({
+		files: ['package.json'],
+		from: /"main": ".*"/g,
+		to: `"main": "dist/index.js"`,
+	});
+}
+
+// prepare();
 module.exports = { prepare };
